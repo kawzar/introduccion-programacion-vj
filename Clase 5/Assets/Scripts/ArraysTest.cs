e35d20b1967abc950f122949e4e7a4d940a07023
kawzar <hmacarena@gmail.com>
2020-03-30 07:03:33 -0300

    Added code for Clase #5

---
+++ Clase 5/Assets/Scripts/ArraysTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysTest : MonoBehaviour
+{
+    public Vector3 potionPosition;
+    public float spaceBetweenPotions = 1;
+
+    public int numberOfPotions = 10;
+
+    public float minYPosition = -4;
+    public float maxYPosition = -2;
+
+    //private int[] numeros;
+    public Potion[] potions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //    numeros = new int[5];
+
+        //    for (int i = 0; i < numeros.Length; i++)
+        //    {
+        //        numeros[i] = i * 2;
+        //        print(numeros[i]);
+        //    }
+
+        //Potion newPotion = Instantiate<Potion>(potions[0]);
+        //newPotion.transform.position = potionPosition;
+
+        for (int i = 0; i < numberOfPotions; i++)
+        {
+            int randomNumber = Random.Range(0, potions.Length);
+            Potion newPotion = Instantiate<Potion>(potions[randomNumber]);
+            newPotion.transform.position = potionPosition;
+
+            potionPosition.x += spaceBetweenPotions;
+            potionPosition.y = Random.Range(minYPosition, maxYPosition);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Clase 5/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float posX = transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+        }
+    }
+}
